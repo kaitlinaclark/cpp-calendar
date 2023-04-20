c0b20b847f278b19218f7c9975464b94a8ac095d
Kaitlin A Clark <kaitlinaclark@outlook.com>
2023-04-20 18:27:44 -0500

    removing redundant files

--- temp/headerone.cpp
+++
@@ -1,9 +0,0 @@
-#include "headerone.h"
-#include <iostream>
-
-using namespace std;
-
-HeaderOne::HeaderOne(string n)
-    : age(0) {
-        cout << "my name is " << n << " and I am " << age << "years old" << endl;
-}
