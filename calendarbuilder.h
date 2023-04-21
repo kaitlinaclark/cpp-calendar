918c8f7b5f127fb46ca28ba759fadb6730197beb
Kaitlin A Clark <kaitlinaclark@outlook.com>
2023-04-20 20:24:05 -0500

    removing parent calendarbuild class

--- calendarbuilder.h
+++ calendarbuilder.h
@@ -8,34 +8,26 @@ which builds all components of the calendar at once.
 #include <ctime>
 #include <memory>
 
+using namespace std;
+
 class Calendar;
 
-class CalendarBuilder {
-public:
-	virtual std::shared_ptr<DisplayableEvent> buildEvent(std::tm when, std::shared_ptr<DisplayableComponent> cal, std::string name, int recurrEvery, int recurrFor, std::string calname) = 0;
-	virtual std::shared_ptr<DisplayableComponent> getComponentByDate(std::tm d, std::string granularity) = 0;
-	virtual std::shared_ptr<Calendar> buildCalendar(std::string name, size_t years) = 0;
-protected:
-	// the Calendar object the builder is currently constructing and managing. Each of the above functions 
-	// takes a shared_ptr to the calendar as an argument, but let's go ahead and store it anyway
-	std::shared_ptr<Calendar> currentCalendar;
-
-	// functions to build components of the calendar
-	// these will be used internally only, so keep encapsulated
-	virtual std::shared_ptr<DisplayableComponent> buildYear(std::tm d, std::shared_ptr<DisplayableComponent> p) = 0;
-	virtual std::shared_ptr<DisplayableComponent> buildDay(std::tm d, std::shared_ptr<DisplayableComponent> p) = 0;
-	virtual std::shared_ptr<DisplayableComponent> buildMonth(std::tm d, std::shared_ptr<DisplayableComponent> p) = 0;
-};
 
 // Concrete builder class
 // builds a full calendar, all at once
-class FullCalendarBuilder : public CalendarBuilder {
+class FullCalendarBuilder {
 public:
-	virtual std::shared_ptr<DisplayableEvent> buildEvent(std::tm when, std::shared_ptr<DisplayableComponent>,std::string name,  int recurrEvery, int recurrFor, std::string calname) override;
-	virtual std::shared_ptr<DisplayableComponent> getComponentByDate(std::tm d, std::string granularity) override;
-	virtual std::shared_ptr<Calendar> buildCalendar(std::string name, size_t years) override;
+	shared_ptr<DisplayableEvent> buildEvent(tm when, shared_ptr<DisplayableComponent>,string name,  int recurrEvery, int recurrFor, string calname);
+	shared_ptr<DisplayableComponent> getComponentByDate(tm d, string granularity);
+	shared_ptr<Calendar> buildCalendar(string name, size_t years);
 protected:
-	virtual std::shared_ptr<DisplayableComponent> buildYear(std::tm d, std::shared_ptr<DisplayableComponent> p) override;
-	virtual std::shared_ptr<DisplayableComponent> buildDay(std::tm d, std::shared_ptr<DisplayableComponent> p) override;
-	virtual std::shared_ptr<DisplayableComponent> buildMonth(std::tm d, std::shared_ptr<DisplayableComponent> p) override;
+	// the Calendar object the builder is currently constructing and managing. Each of the above functions 
+	// takes a shared_ptr to the calendar as an argument, but let's go ahead and store it anyway
+	shared_ptr<Calendar> currentCalendar;
+
+	// functions to build components of the calendar
+	// these will be used internally only
+	shared_ptr<DisplayableComponent> buildYear(tm d, shared_ptr<DisplayableComponent> p);
+	shared_ptr<DisplayableComponent> buildDay(tm d, shared_ptr<DisplayableComponent> p);
+	shared_ptr<DisplayableComponent> buildMonth(tm d, shared_ptr<DisplayableComponent> p);
 };
